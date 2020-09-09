a54fea9d9a54bd3c357f178eccbfe7a4d3f8e62d
joserosalesdh <jm.rosales92@gmail.com>
2020-09-09 17:46:07 -0300

    poniendo rutas

---
+++ src/JournalApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/styles.scss';
+import AppRouter from './routers/AppRouter';
+
+const JournalApp = () => {
+    return (
+
+        <AppRouter />
+
+    )
+}
+
+export default JournalApp
